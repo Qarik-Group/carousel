12a193a7224eb19a203d89a898c1193c763fe198
rkoster <rkoster@starkandwayne.com>
2021-02-25 22:03:20 +0100

    {ca,leaf}-certificates commands should go to help when invoked directly

--- cmd/ca_certificates.go
+++ cmd/ca_certificates.go
@@ -16,37 +16,17 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 // caCertificatesCmd represents the caCertificates command
 var caCertificatesCmd = &cobra.Command{
 	Use:     "ca-certificates",
-	Short:   "A brief description of your command",
+	Short:   "Perform actions on CA Certificates",
+	Long:    "CA Certificates related actions.",
 	Aliases: []string{"ca"},
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("caCertificates called")
-	},
 }
 
 func init() {
 	rootCmd.AddCommand(caCertificatesCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// caCertificatesCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// caCertificatesCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/leaf_certificates.go
+++ cmd/leaf_certificates.go
@@ -16,37 +16,17 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 // leafCertificatesCmd represents the leafCertificates command
 var leafCertificatesCmd = &cobra.Command{
 	Use:     "leaf-certificates",
-	Short:   "A brief description of your command",
+	Short:   "Perform actions on Leaf Certificates",
+	Long:    "Leaf Certificates related actions.",
 	Aliases: []string{"leaf"},
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("leafCertificates called")
-	},
 }
 
 func init() {
 	rootCmd.AddCommand(leafCertificatesCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// leafCertificatesCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// leafCertificatesCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
